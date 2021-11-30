2afe7c94bb779908da53bf796b19371e7eafbc9b
Chiara <dulcechiara@gmail.com>
2021-11-30 16:33:54 -0300

    fix conflict

---
+++ build/precache-manifest.3aec584abc7b0420a48ca9a8e3161fb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e1807dc9b864bc09c6be5b88093a6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98c1de54df795821152d",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "8a9d990cb068a1a37920",
+    "url": "/static/css/main.f2068fec.chunk.css"
+  },
+  {
+    "revision": "98c1de54df795821152d",
+    "url": "/static/js/2.8418e51f.chunk.js"
+  },
+  {
+    "revision": "4d7a8478ab31ae8cb535e6a9db2c9460",
+    "url": "/static/js/2.8418e51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9d990cb068a1a37920",
+    "url": "/static/js/main.aefaa803.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  }
+]);
